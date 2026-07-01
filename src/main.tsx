4a36d486e85aa76c65f795d97646f54920a394a4
favour <femmaje@gmail.com>
2026-07-01 22:11:00 +0100

    feat: add agentation package and integrate into main application

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router'
 import './index.css'
 import { TRPCProvider } from "@/providers/trpc"
+import { Agentation } from 'agentation'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <HashRouter>
       <TRPCProvider>
         <App />
+        {import.meta.env.DEV && <Agentation />}
       </TRPCProvider>
     </HashRouter>
   </StrictMode>,
